b976cd169e13223dc96e73105c4d10ea4f191633
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2021-10-18 18:09:58 +0300

    on the way to implement add to basket method

---
+++ Web/LibraryShop.Web/Controllers/BasketController.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryShop.Web.Controllers
+{
+    using LibraryShop.Services.Data.BasketService;
+    using LibraryShop.Web.ViewModels.Basket;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BasketController : Controller
+    {
+        private readonly IBasketService basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            this.basketService = basketService;
+        }
+
+        [Authorize]
+        public IActionResult MyBasket(BaskteViewModel basketView)
+        {
+            return this.View(basketView);
+        }
+    }
+}
