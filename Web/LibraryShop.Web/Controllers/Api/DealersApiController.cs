abed18e721047cd03a8cffec6863001ff6c09eaa
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2021-11-02 18:30:59 +0200

    Added Api/DealersApiController.cs

---
+++ Web/LibraryShop.Web/Controllers/Api/DealersApiController.cs
@@ -0,0 +1,28 @@
+﻿namespace LibraryShop.Web.Controllers.Api
+{
+    using System.Collections.Generic;
+
+    using LibraryShop.Common;
+    using LibraryShop.Services.Data.Dealer;
+    using LibraryShop.Web.ViewModels.Dealer;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/dealers")]
+    public class DealersApiController : ControllerBase
+    {
+        private readonly IDealerService dealerService;
+
+        public DealersApiController(IDealerService dealerService)
+        {
+            this.dealerService = dealerService;
+        }
+
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IEnumerable<AllDealersModel> All()
+        {
+            return this.dealerService.GetAll();
+        }
+    }
+}
