a86b173592332f5a21c9f460e3abcdad11c34abf
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2021-11-02 18:12:27 +0200

    Added ApiBookController

---
+++ Web/LibraryShop.Web/Controllers/Api/BooksApiController.cs
@@ -0,0 +1,25 @@
+﻿namespace LibraryShop.Web.Controllers.Api
+{
+    using LibraryShop.Services.Data.BookService;
+    using LibraryShop.Web.ViewModels.Book;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/books")]
+    public class BooksApiController : ControllerBase
+    {
+        private readonly IBookService bookService;
+
+        public BooksApiController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        [HttpGet]
+        public ServiceModelSorting GetAll([FromQuery] AllBooksListingAndSortingViewModel query)
+        {
+           return this.bookService.All(
+                query.Author, query.Gener, query.Title, query.SearchTherm, query.Sorting, query.CurrentPage, AllBooksListingAndSortingViewModel.BooksPerPage);
+        }
+    }
+}
